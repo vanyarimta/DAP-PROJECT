95e317d7c3123f5c1dfa2516637f474427bcae09
vanyarimta <82896746+vanyarimta@users.noreply.github.com>
2024-12-19 14:49:06 +0700

    Code.R

--- Code.R
+++ Code.R
@@ -1,7 +1,8 @@
 
+
 ###~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~Import Data#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 getwd()
-data = read.csv('/Users/promac/Downloads/Predicting-Satisfaction-Of-Airline-Customers-master/airlinedata.csv')
+data = read.csv('/Users/promac/Downloads/DAP PROJECT FIX/airlinedata.csv')
 
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~Data Exploration#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 str(data)
@@ -52,7 +53,6 @@ melted_cormat <- melt(realmatrix)
 head(melted_cormat)
 
 #correlation heatmap
-library(ggplot2)
 ggplot(data = melted_cormat, aes(x=Var1, y=Var2, fill=value)) + 
   geom_tile(color = 'white')+
   scale_fill_gradient2(low = "blue", high = "red", mid = "white", 
@@ -79,6 +79,7 @@ colSums(is.na(data))
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~Imputing missing values ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 data = data %>%
   mutate(Arrival.Delay.in.Minutes= coalesce(Arrival.Delay.in.Minutes ,Departure.Delay.in.Minutes))
+colSums(is.na(data))
 
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~Removal of highly correlated features~~~~~~~~~~~~~~~~~~~~~
 data= data%>%select(-'Arrival.Delay.in.Minutes')
@@ -159,7 +160,7 @@ confusionMatrix(cm)
 
 #building svm model with rbf kernel 
 classifier_rbf = ksvm(satisfaction~., data = training_set,
-                      kernel = 'rbfdot')  
+                  kernel = 'rbfdot')  
 classifier_rbf
 
 #predicting the model with training data
@@ -178,7 +179,7 @@ confusionMatrix(cm_rbf)
 
 #building svm model with tanhdot
 classifier_tanhdot = ksvm(satisfaction~., data = training_set,
-                          kernel = 'tanhdot')  
+                      kernel = 'tanhdot')  
 classifier_tanhdot
 
 #predicting the model with training data
@@ -219,7 +220,7 @@ confusionMatrix(cm_polydot)
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~Model Tuning 1 ~~~~~~~~~~~~~~~~~~~~~
 #building svm model with rbf kernel + increase Cost parameter to 3
 classifier_rbf_cost3= ksvm(satisfaction~., data = training_set,
-                           kernel = 'rbfdot', C = 3)  
+                      kernel = 'rbfdot', C = 3)  
 classifier_rbf_cost3
 
 #Tuned Model Testing
@@ -246,188 +247,3 @@ plot(roc_svm_test, add = TRUE,col = "red", print.auc=TRUE, print.auc.x = 0.5, pr
 legend(0.3, 0.2, legend = c("test-svm"), lty = c(1), col = c("blue"))
 
 pred_ROCR <- prediction(training_set$satisfaction, classifier_rbf_cost3)
-
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~Normalizing Data Before Building ANN~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-#normalizing the data
-normalize = function(x) {
-  return((x- min(x))/ (max(x) - min(x)))
-}
-
-#convert the all variables to numeric in order to be normalized
-training_set_converted = training_set%>%mutate(Gender = as.numeric(Gender),
-                                               Customer.Type =as.numeric(Customer.Type),
-                                               Type.of.Travel = as.numeric(Type.of.Travel),
-                                               Class = as.numeric(Class),
-                                               Inflight.wifi.service = as.numeric(Inflight.wifi.service),
-                                               Departure.Arrival.time.convenient = as.numeric(Departure.Arrival.time.convenient),
-                                               Ease.of.Online.booking = as.numeric(Ease.of.Online.booking),
-                                               Gate.location = as.numeric(Gate.location),
-                                               Food.and.drink = as.numeric(Food.and.drink),
-                                               Online.boarding = as.numeric(Online.boarding),
-                                               Seat.comfort = as.numeric(Seat.comfort),
-                                               Inflight.entertainment = as.numeric(Inflight.entertainment),
-                                               On.board.service = as.numeric(On.board.service),
-                                               Leg.room.service = as.numeric(Leg.room.service),
-                                               Baggage.handling = as.numeric(Baggage.handling),
-                                               Checkin.service = as.numeric(Checkin.service),
-                                               Inflight.service = as.numeric(Inflight.service),
-                                               Cleanliness = as.numeric(Cleanliness),
-                                               satisfaction = as.numeric(satisfaction))
-
-test_set_converted = test_set%>%mutate(Gender = as.numeric(Gender),
-                                       Customer.Type =as.numeric(Customer.Type),
-                                       Type.of.Travel = as.numeric(Type.of.Travel),
-                                       Class = as.numeric(Class),
-                                       Inflight.wifi.service = as.numeric(Inflight.wifi.service),
-                                       Departure.Arrival.time.convenient = as.numeric(Departure.Arrival.time.convenient),
-                                       Ease.of.Online.booking = as.numeric(Ease.of.Online.booking),
-                                       Gate.location = as.numeric(Gate.location),
-                                       Food.and.drink = as.numeric(Food.and.drink),
-                                       Online.boarding = as.numeric(Online.boarding),
-                                       Seat.comfort = as.numeric(Seat.comfort),
-                                       Inflight.entertainment = as.numeric(Inflight.entertainment),
-                                       On.board.service = as.numeric(On.board.service),
-                                       Leg.room.service = as.numeric(Leg.room.service),
-                                       Baggage.handling = as.numeric(Baggage.handling),
-                                       Checkin.service = as.numeric(Checkin.service),
-                                       Inflight.service = as.numeric(Inflight.service),
-                                       Cleanliness = as.numeric(Cleanliness),
-                                       satisfaction = as.numeric(satisfaction))
-
-#normalize training and test 
-training_set_normalized =as.data.frame(lapply(training_set_converted[,c(1:22)], normalize))
-str(training_set_normalized)
-
-test_set_normalized =as.data.frame(lapply(test_set_converted[,c(1:22)], normalize))
-str(test_set_normalized)
-
-#convert "satisfaction" back to factor class
-training_set_normalized$satisfaction = as.factor(training_set_normalized$satisfaction)
-test_set_normalized$satisfaction = as.factor(test_set_normalized$satisfaction)
-
-
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Building ANN using H2o package~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-#building model with h2o package
-install.packages('h2o')
-library(h2o)
-library(caret)
-
-#Start a local cluster with 6GB RAM and all the cores"
-h2o.init(nthreads = -1, max_mem_size = '6G')
-
-
-#converting training and test set into H2o dataframe
-train = as.h2o(training_set_normalized)
-test = as.h2o(test_set_normalized)
-str(train)
-train
-#take a look at the contents of the h2oframe(train and test set)
-h2o.describe(train)
-h2o.describe(test)
-View(train)
-
-
-#building neural network using H2o package, ReClu function
-# 1 layers with 5 neurons  and 100 iterations (epoch), 
-n = h2o.deeplearning(x = 1:21,
-                     y = 'satisfaction',
-                     training_frame = train,
-                     standardize = FALSE,
-                     activation = 'RectifierWithDropout',
-                     hidden = 5,
-                     seed = 123,
-                     epochs = 100)
-
-
-#compute variable importance and performance
-h2o.varimp_plot(n,num_of_features = 20)
-
-#evaluate the performance of the model  
-h2o.performance(n)
-
-#testing the model using train set 
-pred = h2o.predict(object = n, 
-                   newdata = train
-)
-
-#convert from h2o frame into dataframe
-predtrain = as.data.frame(pred)
-trainindf = as.data.frame(train)
-View(predtrain)
-View(trainindf)
-
-cm_train_nn = confusionMatrix(table(predtrain$predict, trainindf$satisfaction))
-cm_train_nn
-
-
-#testing the model using test set 
-pred_test = h2o.predict(object = n, 
-                        newdata = test
-)
-
-predtest = as.data.frame(pred_test)
-testindf = as.data.frame(test)
-View(predtest)
-View(testindf)
-
-cm_test_nn = confusionMatrix(table(predtest$predict, testindf$satisfaction))
-cm_test_nn
-
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~Model Tuning~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-#IMPROVING THE MODEL , ReClu function
-#2 layers with 20 neurons each , 3 fold cross validation and 1000 iterations (epoch)
-nimproved = h2o.deeplearning(x = 1:22,
-                             y = 'satisfaction',
-                             training_frame = train,
-                             standardize = FALSE,
-                             nfolds = 3,
-                             activation = 'RectifierWithDropout',
-                             hidden = c(20, 20),
-                             seed = 123,
-                             epochs = 1000)
-
-
-#IMPROVED MODEL TESTINGGG
-#compute variable importance and performance
-h2o.varimp_plot(nimproved,num_of_features = 20)
-
-#evaluate the performance of the model  
-h2o.performance(nimproved)
-
-
-#testing the model using train set 
-pred_nnimproved_trainingset = h2o.predict(object = nimproved, 
-                                          newdata = train
-)
-
-
-pred_train_nnimproved = as.data.frame(pred_nnimproved_trainingset)
-trainindf = as.data.frame(train)
-View(predtrain)
-View(trainindf)
-
-cm_train_nnimproved = confusionMatrix(table(pred_train_nnimproved$predict, trainindf$satisfaction))
-cm_train_nnimproved 
-
-#explore predictions in test set 
-pred_nnimproved_testset= h2o.predict(object = nimproved, 
-                                     newdata = test
-)
-pred_test_nnimproved = as.data.frame(pred_nnimproved_testset)
-testindf = as.data.frame(test)
-View(predindf)
-View(testindf)
-
-cm_test_nnimproved  = confusionMatrix(table(pred_test_nnimproved$predict, testindf$satisfaction))
-cm_test_nnimproved 
-
-h2o.auc(nimproved)
-h2o.auc(n)
-
-h2o.shutdown()
-Y
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~END~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
